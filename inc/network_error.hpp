d600a2047fd178777c92bcc1b704e572a63fdc4a
Mitya <tristan.it@outlook.com>
2022-05-30 17:01:30 +0200

    implement error_code with internal error codes

---
+++ inc/network_error.hpp
@@ -0,0 +1,37 @@
+#ifndef NETWORK_ERROR_HPP
+#define NETWORK_ERROR_HPP
+
+#include <system_error>
+#include <cstdint>
+
+namespace tristan::network{
+
+    enum class ErrorCode : uint8_t{
+        SUCCESS,
+        OFFLINE,
+        INVALID_URL,
+        HOST_NOT_FOUND,
+        FILE_PATH_EMPTY
+    };
+
+    /**
+     * \brief Creates std::error_code object that stores error information.
+     * \param error_code ErrorCode
+     * \return std::error_code
+     */
+    auto makeError(ErrorCode error_code) -> std::error_code;
+
+} //End of tristan::network namespace
+
+namespace std{
+
+    /**
+   * \brief //This is needed to specialise the standard type trait.
+   */
+    template<>
+    struct is_error_code_enum<tristan::network::ErrorCode> : true_type{
+    };
+
+} //End of std namespace
+
+#endif //NETWORK_ERROR_HPP
---
+++ src/network_error.cpp
@@ -0,0 +1,48 @@
+#include "network_error.hpp"
+
+#include <array>
+#include <string>
+
+namespace /*anonymous*/
+{
+
+    /**
+   * \private
+   * \brief Struct to overload std::error_category methods
+   */
+    struct NetworkCategory : std::error_category{
+        [[nodiscard]] const char* name() const noexcept override;
+
+        [[nodiscard]] std::string message(int ec) const override;
+    };
+
+    inline const NetworkCategory g_network_error_category;
+
+    inline const uint8_t g_max_error_code = 5;
+
+    inline const std::array<const char*, g_max_error_code> g_error_code_descriptions{
+        "Success",
+        "Network offline",
+        "Invalid Url",
+        "Remote host not found",
+        "Output file path is empty"
+    };
+
+} //End of /*anonymous*/ namespace
+
+auto tristan::network::makeError(tristan::network::ErrorCode error_code) -> std::error_code{
+    return { static_cast<int>(error_code),
+             g_network_error_category
+    };
+}
+
+namespace /*anonymous*/
+{
+    auto NetworkCategory::name() const noexcept -> const char*{
+        return "TristanNetworkCategory";
+    }
+
+    auto NetworkCategory::message(int ec) const -> std::string{
+        return {g_error_code_descriptions.at(ec)};
+    }
+} //End of /*anonymous*/ namespace
